ffe6328f8697e138ad5b1e55059f0052aba851aa
NicolasRenson <nicolasrenson48@gmail.com>
2018-03-14 16:09:51 +0100

    J'ai fni le exo1

---
+++ main.ino
@@ -0,0 +1,19 @@
+//  Copyright (c) 2018 Antoine Tran Tan
+//
+
+#include "Arduino.h"
+
+// the setup function runs once when you press reset or power the board
+void setup()
+{
+    pinMode(13,OUTPUT);
+}
+
+// the loop function runs over and over again forever
+void loop()
+{
+    digitalWrite(13,HIGH);
+    delay(2000);
+    digitalWrite(13,LOW);
+    delay(2000);
+}
